5660f8db5b7d83ad7eb59afa6935dc10bdcab49b
pravallika-0201 <golipravallika6@gmail.com>
2025-05-20 14:47:20 +0530

    final

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# # Route for the home page (root URL)
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
+# Route for the home page (root URL)
+@app.route('/')
+def home():
+    return render_template('bootstrapdemo.html')
 
-# # Route for index page (optional if you want a separate path)
-# @app.route('/index')
-# def index():
-#     return render_template('index.html')
+# Route for index page (optional if you want a separate path)
 
 
 if __name__ == '__main__':
